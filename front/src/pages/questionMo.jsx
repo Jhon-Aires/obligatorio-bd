851c5a0100c309195848f0a2e52c7107ddfeaebb
Santiago Acosta <santiacosta2499@gmail.com>
2025-05-25 21:05:45 -0300

    front

---
+++ front/src/pages/questionMo.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const QuestionMO = () => {
+return(
+<div>
+    <h1>QuestionMO</h1>
+    <input placeholder = "ingrece su usuario"></input>
+    {/* <button onClick={link='./home'}>Ingresar</button> */}
+</div>
+)
+}
+export default QuestionMO;
---
+++ front/src/pages/questionText.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const QuestionText = () => {
+return(
+<div>
+    <h1>Question</h1>
+    <input placeholder = "ingrese su usuario"></input>
+    {/* <button onClick={link='./home'}>Ingresar</button> */}
+</div>
+)
+}
+export default QuestionText;

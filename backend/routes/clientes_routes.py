cfb000cf60547df68e041230e1f5a35a1e798433
luvalciukas <lvalciukas2003@gmail.com>
2025-06-01 10:11:43 -0300

    delete y patch basico

--- backend/routes/clientes_routes.py
+++ backend/routes/clientes_routes.py
@@ -11,7 +11,7 @@ def listar_clientes():
     resultado = cursor.fetchall()
     cursor.close()
     conn.close()
-    return jsonify(resultado)
+    return jsonify(resultado), 200
 
 @clientes_bp.route('/', methods=['POST'])
 def crear_cliente():
@@ -19,11 +19,42 @@ def crear_cliente():
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO clientes (nombre, direccion, contacto, correo) VALUES (%s, %s, %s)",
+        "INSERT INTO clientes (nombre, direccion, contacto, correo) VALUES (%s, %s, %s, %s)",
         (datos['nombre'], datos['direccion'], datos['contacto'], datos['correo'])
     )
     conn.commit()
     cursor.close()
     conn.close()
     return jsonify({"mensaje": "cliente creado"}), 201
+
+#Version basica, el json debe darle el id coincidente y los datos nuevos que quiere editar para luego editarlos
+#ventaja: se puede editar todo de una, desventaja: se debe tener todos los datos si se quiere editar una cosa sola
+@clientes_bp.route('/', methods=['PATCH'])
+def editar_cliente():
+    datos = request.json
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE clientes SET nombre = %s, direccion = %s, contacto = %s, correo = %s WHERE id = %s",
+        (datos['nombre'], datos['direccion'], datos['contacto'], datos['correo'], datos ['id'])
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"mensaje": "Cliente modificado"}), 204
+
+#Se permite borrar solo por ID
+@clientes_bp.route('/', methods=['DELETE'])
+def eliminar_cliente():
+    datos = request.json
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "DELETE FROM clientes WHERE id= %d" %
+        (datos['id'])
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"mensaje": "cliente eliminado"}), 204
 

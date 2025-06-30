028b9314ec4a8293b5e25384e0088c48ec900c05
Jhon-Aires <jl.airesoxley@gmail.com>
2025-06-29 21:28:24 -0300

    fix host on privileges

--- db_init/init.sql
+++ db_init/init.sql
@@ -113,16 +113,16 @@ INSERT INTO registro_consumo (id, id_maquina_en_uso, id_insumo, fecha, cantidad_
     ('rc-003', 1, 3, '2025-05-12', 10);
 
 -- Crear los usuarios de MySQL
-CREATE USER 'admin_user'@'localhost' IDENTIFIED BY 'adminpass';
-CREATE USER 'limited_user'@'localhost' IDENTIFIED BY 'userpass';
+CREATE USER 'admin_user'@'%' IDENTIFIED BY 'adminpass';
+CREATE USER 'limited_user'@'%' IDENTIFIED BY 'userpass';
 
 -- Dar acceso total sobre las tablas de marloy
-GRANT ALL PRIVILEGES ON marloy.* TO 'admin_user'@'localhost';
+GRANT ALL PRIVILEGES ON marloy.* TO 'admin_user'@'%';
 
 -- Dar acceso limitado al otro usuario
-GRANT SELECT, INSERT, UPDATE, DELETE ON marloy.insumos TO 'limited_user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON marloy.clientes TO 'limited_user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON marloy.mantenimientos TO 'limited_user'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON marloy.insumos TO 'limited_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON marloy.clientes TO 'limited_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON marloy.mantenimientos TO 'limited_user'@'%';
 
 -- Aplicar cambios
 FLUSH PRIVILEGES;
